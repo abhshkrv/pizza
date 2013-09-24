a89a9bcdb386f530d26bbc80373306b4f101123e
Abhishek <abhishekravi1992@gmail.com>
2013-09-25 02:20:41 +0800

    Weird, outlet.cs is not commited

---
+++ HQServer/HQServer.Domain/Outlet.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HQServer.Domain.Entities
+{
+    public class Outlet
+    {
+        public int outletID { get; set; }
+        public string owner { get; set; }
+        public string address { get; set; }
+
+    }
+}
