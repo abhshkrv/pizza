aad7d784da12c4e7023c0957b2a0eb8dc8d131a9
Abhishek <abhishekravi1992@gmail.com>
2013-12-05 12:23:45 +0800

    Batch controller

---
+++ HQServer/HQServer.WebUI/Controllers/BatchController.cs
@@ -0,0 +1,58 @@
+﻿using HQServer.Domain.Abstract;
+using HQServer.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HQServer.WebUI.Controllers
+{
+    public class BatchController : Controller
+    {
+        IBatchResponseRepository _batchResponseRepo;
+        IProductRepository _productRepo;
+        IBatchResponseDetailRepository _batchResponseDetailRepo;
+        public BatchController(IBatchResponseRepository brepo, IBatchResponseDetailRepository bdrepo, IProductRepository prepo)
+        {
+
+            _productRepo = prepo;
+            _batchResponseRepo = brepo;
+            _batchResponseDetailRepo = bdrepo;
+        }
+
+        [HttpPost]
+        public ContentResult Send(string shopID, string barcode, string qty, string requestID)
+        {
+            Product product = _productRepo.Products.FirstOrDefault(p => p.barcode == barcode);
+            if (product == null)
+            {
+                ViewBag["Result"] = "Error";
+                return View();
+            }
+            else
+            {
+                BatchResponse batchResponse = new BatchResponse();
+                BatchResponseDetail batchResponseDetail = new BatchResponseDetail();
+                batchResponse.status = Status.NOT_RESPONDED;
+                batchResponse.timestamp = DateTime.Now;
+                batchResponse.requestID = Int16.Parse(requestID);
+
+                _batchResponseRepo.saveBatchResponse(batchResponse);
+                batchResponseDetail.requestID = batchResponse.requestID;
+                batchResponseDetail.barcode = Int32.Parse(barcode);
+                batchResponseDetail.quantity = Int32.Parse(qty);
+
+
+
+                return View();
+            }
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
